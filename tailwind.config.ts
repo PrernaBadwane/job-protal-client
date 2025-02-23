f72e114399eab8b61d965d30672d38130c1d5f88
PrernaBadwane <95902077+PrernaBadwane@users.noreply.github.com>
2025-02-23 21:48:41 +0530

    favicon and title updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,15 @@ export default {
     extend: {
       colors: {
         primary:{
-          10: "#000000",
+          10: "#3D52A0",
+        },
+        secondary:{
+          10: "#7091E6",
+        },
+        accent:{
+          10: "#8697C4",
+          20:"#ADBBDA",
+          30:"#EDE8F5"
         }
       },
     },
